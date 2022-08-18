af52f1afee1dff3b61fd6e829988b3ae68b435ce
Bullet Rout <routbullet@gmail.com>
2022-08-18 14:49:26 +0530

    Perform index at method

--- linked list/singlyLinkedList.js
+++ linked list/singlyLinkedList.js
@@ -20,7 +20,7 @@ class LinkedList{
 
     // insert at end
     insertAtEnd(data){
-        let lastNode = new Node(data)
+        let lastNode = new Node(data, null)
 
         // if there is no node then we will make it our first node
         if(this.head === null){
@@ -36,10 +36,40 @@ class LinkedList{
             current.next = lastNode
             this.size++
         }
-
     }
 
     // insert at index
+
+    insertAtIndex(data, index){
+        let indexedNode = new Node(data)
+
+        //if the linked-list size is less than index(out of range)
+        if(index > 0 && index > this.size){
+            return;
+        }
+
+        // if the index value is zero(first index)
+        if(index === 0){
+            this.head = new Node(data, this.head)
+            this.size++
+            return;
+        }
+
+        // if index within range
+        let current = this.head
+        let prevNode;
+        let count = 0
+        while(count < index){
+            prevNode = current  //the node before index
+            count++
+            current = current.next // the node after index
+        }
+
+        indexedNode.next = current;
+        prevNode.next = indexedNode;
+        this.size++
+        
+    }
 
     // get a index
 
@@ -70,10 +100,13 @@ class LinkedList{
 
 const objLL = new LinkedList();
 
-objLL.insertAtStart(100)
-objLL.insertAtStart(200)
-objLL.insertAtStart(300)
-objLL.insertAtEnd(50)
+// objLL.insertAtStart(100)
+// objLL.insertAtStart(200)
+// objLL.insertAtStart(300)
+// objLL.insertAtEnd(50)
+// console.log(objLL)    
+// objLL.insertAtEnd(60)
+// objLL.insertAtStart(400)
+// objLL.insertAtIndex(150,0)
 console.log(objLL)    
-
 objLL.displayList()    //output: 300 ==> 200 ==> 100 ==> null
