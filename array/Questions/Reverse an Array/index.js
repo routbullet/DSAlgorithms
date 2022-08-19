20eb67ec62ad35c8b9e90dcbed0a76d8a39a0c07
Bullet Rout <routbullet@gmail.com>
2022-08-19 20:13:25 +0530

    perform reverse the array with method 2

--- array/Questions/Reverse an Array/index.js
+++ array/Questions/Reverse an Array/index.js
@@ -1,16 +1,38 @@
 // Reverse an Array
 
 //method 1
+
+// const reverseArr = (arr) => {
+//     let rev = []
+//     for(let i = arr.length-1; i>= 0; i--){
+//         rev.push(arr[i]);
+//     }
+//     return rev;
+
+// }
+
+// arr = [1, 2, 3, 4, 5]
+
+
+// console.log(reverseArr(arr)); //[ 5, 4, 3, 2, 1 ]
+
+
+// -----------------------------------------------------
+
+// method 2
+
 const reverseArr = (arr) => {
-    let rev = []
-    for(let i = arr.length-1; i>= 0; i--){
-        rev.push(arr[i]);
+    for(let i = 0; i < Math.floor(arr.length/2); i++){
+      arr[i] = arr[i] ^ arr[arr.length-1-i]
+      arr[arr.length-1-i] = arr[i] ^ arr[arr.length-1-i]
+      arr[i] = arr[i] ^ arr[arr.length-1-i]
     }
-    return rev;
+    
+    return arr;
 
 }
 
 arr = [1, 2, 3, 4, 5]
 
-
 console.log(reverseArr(arr)); //[ 5, 4, 3, 2, 1 ]
+
