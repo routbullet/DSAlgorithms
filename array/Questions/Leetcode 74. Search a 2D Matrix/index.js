f48bbdd7155d6cf70fb400b317858308da8e812e
Bullet Rout <routbullet@gmail.com>
2022-09-17 14:07:45 +0530

    Leetcode 74. Search a 2D Matrix

---
+++ array/Questions/Leetcode 74. Search a 2D Matrix/index.js
@@ -0,0 +1,35 @@
+/*
+Leetcode 74. Search a 2D Matrix
+
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+*/
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+  let i = 0,
+    j = matrix[0].length - 1;
+  while (i < matrix.length && j >= 0) {
+    if (matrix[i][j] == target) return true;
+    else if (matrix[i][j] > target) j--;
+    else if (matrix[i][j] < target) i++;
+  }
+  return false;
+};
+
+let matrix = [
+    [1, 3, 5, 7],
+    [10, 11, 16, 20],
+    [23, 30, 34, 60],
+  ],
+  target = 3;
+
+console.log(searchMatrix(matrix, target));
